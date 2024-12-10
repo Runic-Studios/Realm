d688f33d17048d4540d8ace7f02f6170b0aa2bd6
Excel619 <excel8392@gmail.com>
2024-12-09 22:59:08 -0500

    Added plugin.yml

--- src/main/kotlin/com/runicrealms/realm/RealmPlugin.kt
+++ src/main/kotlin/com/runicrealms/realm/RealmPlugin.kt
@@ -9,9 +9,7 @@ class RealmPlugin : JavaPlugin() {
     private var injector: Injector? = null
 
     override fun onEnable() {
-        // Create the Guice Injector with the plugin module
         injector = Guice.createInjector(MyPluginModule(this))
-
-        logger.info("MyPlugin has been enabled!")
+        logger.info("Realm Guice injection has finished!")
     }
 }
